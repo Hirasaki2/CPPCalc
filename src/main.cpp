172e9bfce034f259406dc8bd3d6f4391c14cfca5
Ryan H <littlekishi@gmail.com>
2019-01-27 11:26:00 -0600

    added command line arguments

--- src/main.cpp
+++ src/main.cpp
@@ -2,34 +2,52 @@
 #include "../include/calc.h"
 #include "../include/menu.h"
 
+const std::string one = "1";
+const std::string two = "2";
+
 //Declare Functions
 int main(int argc, char *argv[]) {
 	bool restart = true;
-	while (restart == true) {
-		int answer;
-		std::cout << "Choose a option!\n 0. Exit\n 1. Magnitude + Vector\n 2. X + Y";
-		std::cout << std::endl << "Choice: ";
-		if (std::cin >> answer) {
-			if (answer == 0) {
-				restart = false;
-			}
-			else {
-				std::cout << std::endl;
-				if (answer == 1) {
-					menuBoth();
-				}
-				else if (answer == 2) {
-					menuXY();
+	if (argc == 1) {
+		while (restart == true) {
+			int answer;
+			std::cout << "Choose a option!\n 0. Exit\n 1. Magnitude + Vector\n 2. X + Y\n";
+			std::cout << "Choice: ";
+			if (std::cin >> answer) {
+				if (answer == 0) {
+					restart = false;
 				}
 				else {
-					
+					std::cout << std::endl;
+					if (answer == 1) {
+						menuBoth();
+					}
+					else if (answer == 2) {
+						menuXY();
+					}
+					else {
+						
+					}
+					std::cout << std::endl;
 				}
-				std::cout << std::endl;
+			}
+			else {
+				return 1;
 			}
 		}
-		else {
-			return 1;
+	}
+	else if (argv[1] == one) {
+		while (true) {
+			menuBoth();
 		}
+	}
+	else if (argv[1] == two) {
+		while (true) {
+			menuXY();
+		}
+	}
+	else {
+		return 1;
 	}
 	return 0;
 }
