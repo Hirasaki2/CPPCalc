4bd0048678d9bfcafd2182ba20b7b449903e22c7
Ryan H <littlekishi@gmail.com>
2019-01-26 18:46:49 -0600

    Now with CMake!

--- Makefile
+++
@@ -1,6 +0,0 @@
-# build an executable named myprog from myprog.c
-all: math.cpp cpp/*.cpp 
-	g++ -g -Wall math.cpp cpp/*.cpp -o math
-
-clean: 
-	$(RM) math
--- math.cpp
+++ math.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 //Declare Functions
-int main() {
+int main(int argc, char *argv[]) {
 	bool restart = true;
 	while (restart == true) {
 		int answer;
