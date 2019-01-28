cbab0896746c7ee8d68a27b4eac8e7ba415c3adf
Ryan H <littlekishi@gmail.com>
2019-01-28 12:17:39 -0600

    now rounds

--- src/calc.cpp
+++ src/calc.cpp
@@ -1,27 +1,30 @@
 #include <math.h>
 
+double rounder(double input) {
+	return round(input * 1000) / 1000;
+}
+
+
 //Option One
 double calcMagnitude(double x1, double y1) {
 	double xPower = pow (x1, 2);
 	double yPower = pow (y1, 2);
 	
-	return (double) sqrt (xPower + yPower);
+	return (double) rounder(sqrt (xPower + yPower));
 }
 double calcVector(double x, double y) {
 	double done = ( atan (y / x) * 180 ) / M_PI;
 	if (x<0 && y>=0) {done = 90 + done;}
 	else if (x<0 && y<0) {done = 180 + done;}
 	else if (x>=0 && y<0) {done = 270 + done;}
-	return done;
+	return rounder(done);
 }
 //Option Two
 double calcX(double magnitude, double vector) {
 	double cosed = cos((vector * M_PI) / 180);
-	//if (vector<90 && vector>=270) {cosed = -cosed;}
-	return cosed * magnitude;
+	return rounder(cosed * magnitude);
 }
 double calcY(double magnitude, double vector) {
 	double sined = sin((vector * M_PI) / 180);
-	//if (vector>180 && vector<=0) {sined = -sined;}
-	return sined * magnitude;
+	return rounder(sined * magnitude);
 }
