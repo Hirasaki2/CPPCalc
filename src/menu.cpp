302639ebf028938ce2cc0662fc1e65bd38c8d11c
Ryan H <littlekishi@gmail.com>
2019-01-28 12:02:13 -0600

    Added more const!

--- src/menu.cpp
+++ src/menu.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include "../include/calc.h"
 
-const std::string magnitude = "Magnitude: ";
-const std::string vector = "Vector: ";
+const std::string magnitudeString = "Magnitude: ";
+const std::string vectorString = "Vector: ";
+const std::string xString = "X: ";
+const std::string yString = "Y: ";
 
 double doubleCout(std::string lol) {
 	double var;
@@ -12,18 +14,18 @@ double doubleCout(std::string lol) {
 }
 void menuBoth() {
 	double x, y;
-	x = doubleCout("x: ");
-	y = doubleCout("y: ");
+	x = doubleCout(xString);
+	y = doubleCout(yString);
 	std::cout << std::endl;
-	std::cout << magnitude << calcMagnitude(x, y) << std::endl;
-	std::cout << vector << calcVector(x, y) << std::endl;
+	std::cout << magnitudeString << calcMagnitude(x, y) << std::endl;
+	std::cout << vectorString << calcVector(x, y) << std::endl;
 }
 void menuXY() {
 	double magnitudeVar, vectorVar;
-	magnitudeVar = doubleCout(magnitude);
-	vectorVar = doubleCout(vector);
+	magnitudeVar = doubleCout(magnitudeString);
+	vectorVar = doubleCout(vectorString);
 	std::cout << std::endl;
-	std::cout << "x: " << calcX(magnitudeVar, vectorVar) << std::endl;
-	std::cout << "y: " << calcY(magnitudeVar, vectorVar) << std::endl;
+	std::cout << xString << calcX(magnitudeVar, vectorVar) << std::endl;
+	std::cout << yString << calcY(magnitudeVar, vectorVar) << std::endl;
 
 }
